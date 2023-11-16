53bdc286305e34e41328d6095fd194a4ec5d1ce3
Detemann <lucasdetemann12@gmail.com>
2023-11-15 22:10:25 -0300

    adicionando arquivo teste

--- src/main/java/com/sarrussys/bloodguardian/HelloApplication.java
+++ src/main/java/com/sarrussys/bloodguardian/HelloApplication.java
@@ -11,10 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("teste.fxml"));
 
         stage.setTitle("Exemplo JavaFX + Hibernate + PostgreSQL");
         stage.setScene(new Scene(root, 300, 250));
+        stage.setMaximized(true);
         stage.show();
     }
 

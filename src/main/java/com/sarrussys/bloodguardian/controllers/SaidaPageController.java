6bc08683d0733f4dad65ac772546c076f0f82658
Detemann <lucasdetemann12@gmail.com>
2023-11-30 00:07:28 -0300

    fixes tela saida

--- src/main/java/com/sarrussys/bloodguardian/controllers/SaidaPageController.java
+++ src/main/java/com/sarrussys/bloodguardian/controllers/SaidaPageController.java
@@ -1,11 +1,14 @@
 package com.sarrussys.bloodguardian.controllers;
 
 import com.sarrussys.bloodguardian.services.SaidaService;
+import com.sarrussys.bloodguardian.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.MenuItem;
 
 import java.net.URL;
 import java.util.List;
@@ -14,13 +17,18 @@ import java.util.ResourceBundle;
 public class SaidaPageController implements Initializable {
     @FXML
     private PieChart pieChart;
+    @FXML
+    private MenuItem menuBtn;
+    @FXML
+    private MenuItem estoqueBtn;
+
+    private MainMenuController mainMenuController = new MainMenuController();
 
     private SaidaService saidaService = new SaidaService();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         List<Integer> resultado = saidaService.getPorcentageEntradasSaidas();
-
 
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(

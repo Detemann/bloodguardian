ad96efd8175e97c1a59e556771a216b00a9cb2b5
Wandersontr01 <wanderson.franca@aluno.faesa.br>
2023-11-30 07:35:38 -0300

    aaa

--- src/main/java/com/sarrussys/bloodguardian/controllers/CadastroBolsasController.java
+++ src/main/java/com/sarrussys/bloodguardian/controllers/CadastroBolsasController.java
@@ -3,6 +3,9 @@ package com.sarrussys.bloodguardian.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.sarrussys.bloodguardian.models.BolsaSangue;
+import com.sarrussys.bloodguardian.models.TipoSanguineo;
+import com.sarrussys.bloodguardian.repositores.TipoSangueRepository;
 import com.sarrussys.bloodguardian.util.Alerts;
 
 import javafx.event.ActionEvent;
@@ -10,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class CadastroBolsasController implements Initializable {
@@ -23,17 +27,43 @@ public class CadastroBolsasController implements Initializable {
 	private TextField txtValidade;
 	@FXML
 	private Button btnSalvar;
+	@FXML
+	private ComboBox<String> cmbTpSanguineo;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		cmbTpSanguineo.getItems().addAll(
+				"A+",
+				"A-",
+				"B+",
+				"B-",
+				"AB+",
+				"AB-",
+				"O+",
+				"O-"
+		);
+
 	}
-	
+
+	@FXML
 	public void onBtnVoltarAction(ActionEvent event) {
 		MainMenuController main = new MainMenuController();
 	}
 	
 	public void onBtnSalvarAction(ActionEvent event) {
+		String codBolsa = txtCodBolsa.getText();
+		String tipoSanguineoString = cmbTpSanguineo.getItems().toString();
+		TipoSanguineo tipoSanguineo = new TipoSanguineo();
+		System.out.println(codBolsa);
+		System.out.println(tipoSanguineoString);
+
+
+		TipoSangueRepository repository = new TipoSangueRepository();
+
+		String DtColeta = txtDtColeta.getText();
+		String validade = txtValidade.getText();
+
+
+
 	}
 	
 	

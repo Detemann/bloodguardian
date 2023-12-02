3713615c59bf664626bc3411e448efdf65502931
Wandersontr01 <wanderson.franca@aluno.faesa.br>
2023-12-02 17:19:32 -0300

    funcionando

--- src/main/java/com/sarrussys/bloodguardian/controllers/MainMenuController.java
+++ src/main/java/com/sarrussys/bloodguardian/controllers/MainMenuController.java
@@ -1,22 +1,34 @@
 package com.sarrussys.bloodguardian.controllers;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import com.sarrussys.bloodguardian.models.BolsaSangue;
+import com.sarrussys.bloodguardian.repositores.BolsaSangueRepository;
 import com.sarrussys.bloodguardian.util.Alerts;
 import com.sarrussys.bloodguardian.util.Utils;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
+
+    @FXML
+    private BarChart<String, Number> barChart; // Adicionado o BarChart no FXML
 
     @FXML
     private Button btnEstoque;
@@ -30,6 +42,7 @@ public class MainMenuController {
             mainStage.setScene(mainScene);
             mainStage.setTitle("SGBS");
             mainStage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -97,5 +110,111 @@ public class MainMenuController {
             e.printStackTrace();
         }
         Utils.currentStage(event).close();
+    }
+
+
+
+    private void fillBarChart() {
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        BolsaSangueRepository repository = new BolsaSangueRepository();
+
+        // Adiciona dados diretamente
+        series.getData().add(new XYChart.Data<>("A+", repository.quantidadeDeTipo(1)));
+        series.getData().add(new XYChart.Data<>("A-", repository.quantidadeDeTipo(2)));
+        series.getData().add(new XYChart.Data<>("B+", repository.quantidadeDeTipo(3)));
+        series.getData().add(new XYChart.Data<>("B-", repository.quantidadeDeTipo(4)));
+        series.getData().add(new XYChart.Data<>("AB+", repository.quantidadeDeTipo(5)));
+        series.getData().add(new XYChart.Data<>("AB-", repository.quantidadeDeTipo(6)));
+        series.getData().add(new XYChart.Data<>("O+", repository.quantidadeDeTipo(7)));
+        series.getData().add(new XYChart.Data<>("O-", repository.quantidadeDeTipo(8)));
+
+        // Define o título do gráfico
+        barChart.setTitle("Estoque de Sangue");
+
+        // Adiciona os dados ao gráfico
+        barChart.getData().add(series);
+
+        // Itera sobre os pontos de dados e define a cor com base na quantidade
+        for (XYChart.Data<String, Number> data : series.getData()) {
+            double value = data.getYValue().doubleValue();
+            String colorStyle = getColorStyle(value);
+            data.getNode().setStyle(colorStyle);
+        }
+    }
+
+    //GPT
+
+//    private String getColorStyle(double value) {
+//        int maxQuantidade = 50; // Defina o valor máximo desejado para escala de cores
+//
+//        // Calcule a escala com base na quantidade
+//        double scale = Math.min(value / maxQuantidade, 1.0);
+//
+//        // Ajuste as componentes de cor com base na escala e no tom de vermelho desejado
+//        int baseRed = 255; // Defina o tom de vermelho base desejado (0-255)
+//        int red = (int) (baseRed + (255 - baseRed) * scale);
+//        int green = 0; // Sempre zero para garantir a cor vermelha
+//        int blue = 0;  // Sem azul para garantir uma cor vermelha
+//
+//        // Converta para formato hexadecimal
+//        String hexColor = String.format("#%02x%02x%02x", red, green, blue);
+//
+//        // Defina a cor da barra no formato CSS
+//        return String.format("-fx-bar-fill: %s;", hexColor);
+//    }
+
+
+
+    private String getColorStyle(double value) {
+        int maxQuantidade = 50; // Defina o valor máximo desejado para escala de cores
+
+        // Calcule a escala com base na quantidade
+        double scale = Math.min(value / maxQuantidade, 1.0);
+
+        // Defina os intervalos de quantidade e seus tons correspondentes de vermelho
+        int[][] intervalos = {
+                {1, 5, 255},   // Intervalo 1-5: Vermelho claro (255)
+                {6, 15, 200},  // Intervalo 6-11: Vermelho mais escuro (200)
+                {16, 30, 150}, // Intervalo 12-17: Vermelho mais escuro ainda (150)
+                {30, 50, 100}, // Intervalo 12-17: Vermelho mais escuro ainda (100)
+                {50, 500, 50}, // Intervalo 12-17: Vermelho mais escuro ainda (50)
+                // Adicione mais intervalos conforme necessário
+        };
+
+        // Encontre o intervalo correspondente
+        int red = 0;
+        for (int[] intervalo : intervalos) {
+            int inicio = intervalo[0];
+            int fim = intervalo[1];
+            int tomVermelho = intervalo[2];
+
+            if (value >= inicio && value <= fim) {
+                red = tomVermelho;
+                break;
+            }
+        }
+
+        int green = 0; // Sempre zero para garantir a cor vermelha
+        int blue = 0;  // Sem azul para garantir uma cor vermelha
+
+        // Converta para formato hexadecimal
+        String hexColor = String.format("#%02x%02x%02x", red, green, blue);
+
+        // Defina a cor da barra no formato CSS
+        return String.format("-fx-bar-fill: %s;", hexColor);
+    }
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fillBarChart();
     }
 }
